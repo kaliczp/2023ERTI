f78cd52cda2df63013528bf6adda95d6a4c82013
Péter Kalicz <kaliczp@gmail.com>
2023-05-16 09:03:31 +0200

    Look two series in a same zoo plot

--- plotfull.R
+++ plotfull.R
@@ -1,5 +1,9 @@
+min(tv.xts[,1], na.rm = TRUE);max(tv.xts[,2], na.rm = TRUE)
+
 plot.zoo(tv.xts[,1],
-         xaxs = "i", ylab = names(tv.xts[1])
+         xaxs = "i", yaxs = "i",
+         xlab = "", ylab = names(tv.xts[1]),
+         ylim = c(-8.76, -6.16)
          )
 lines(as.zoo(tv.xts[,2]),
       col = 2)
