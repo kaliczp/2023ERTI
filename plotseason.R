718ff5761aad2d27300426e3112ed673d60270b8
Péter Kalicz <kaliczp@gmail.com>
2023-05-16 11:32:16 +0200

    Plot for seasons based on data.frame
    date.df data frame is created with barrier dates.
    
    datelim.part vector is modified for date.df database. Each date can be
    addressed by its index. Following dates are selected automatically.

--- plotseason.R
+++ plotseason.R
@@ -1,3 +1,13 @@
-datelim.part  <- datelim.full <- c(ISOdate(2018,11,1, tz = "UTC"), ISOdate(2019,5,1, tz = "UTC"))
+date.df <- data.frame(yr = c(2018, rep(2019:2022, lenght = 8, each = 2)),
+                      mnth = rep(c(11,5), length = 9)
+                      )
 
+    ttdate <- 1
+    datelim.part <-  c(ISOdate(date.df[ttdate,"yr"],
+                               date.df[ttdate,"mnth"],
+                               1, tz = "UTC"),
+                       ISOdate(date.df[ttdate + 1,"yr"],
+                               date.df[ttdate + 1,"mnth"],
+                               1, tz = "UTC")
+                       )
 plot.fullgw(temp = FALSE, datelim = datelim.part)
