05e3e194278256a7c65da091c6708e8c99ac3562
Péter Kalicz <kaliczp@gmail.com>
2023-05-16 09:15:10 +0200

    Meteo created

--- plotfull.R
+++ plotfull.R
@@ -7,3 +7,15 @@ plot.zoo(tv.xts[,1],
          )
 lines(as.zoo(tv.xts[,2]),
       col = 2)
+
+plot.zoo(met.xts[,3], type = "h",
+         xaxs = "i", yaxs = "i",
+         ylim = c(100,0),
+         col = "blue"
+         )
+par(new = TRUE)
+plot.zoo(met.xts[,4],
+         xaxs = "i", yaxs = "i",
+         ylim = c(-50,50),
+         col = "red"
+         )
