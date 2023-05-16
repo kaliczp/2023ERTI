1a70e98dec84e6bf552e01d95460b0274e09d8d7
Péter Kalicz <kaliczp@gmail.com>
2023-05-16 07:32:17 +0200

    Remove last almost empty row from tv

--- beolvas.R
+++ beolvas.R
@@ -1,3 +1,6 @@
 library(readxl)
 tv.raw <- as.data.frame(read_excel("Alapadatok_Péternek.xlsx"))
 met.raw <- as.data.frame(read_excel("Alapadatok_Péternek.xlsx", 2))
+
+## Remove last almost empty rows
+tv.raw <- tv.raw[-(138176:138178),]
