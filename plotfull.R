ce93daff0499ba3ca55cc76e520fa188c758a937
Péter Kalicz <kaliczp@gmail.com>
2023-05-16 09:40:21 +0200

    Save pdf with right axis labs

--- plotfull.R
+++ plotfull.R
@@ -2,6 +2,7 @@ Sys.setenv(TZ = "UTC")
 min(tv.xts[,1], na.rm = TRUE);max(tv.xts[,2], na.rm = TRUE)
 datelim <- c(ISOdate(2018,11,1, tz = "UTC"), ISOdate(2022,10,10, tz = "UTC"))
 
+pdf(width = 14)
 par(mar = c(3.1, 4.1, 0.6, 4.1))
 plot.zoo(tv.xts[,1],
          xaxs = "i", yaxs = "i",
@@ -19,6 +20,7 @@ plot.zoo(met.xts[,3], type = "h", lend = 2,
          col = "blue"
          )
 axis(4, at = c(0,10,20), col = "blue")
+mtext("Prec. [mm/day]", 4, at = 10, col = "blue", line = 3)
 par(new = TRUE)
 plot.zoo(met.xts[,4],
          xaxs = "i", yaxs = "i",
@@ -28,3 +30,5 @@ plot.zoo(met.xts[,4],
          col = "red"
          )
 axis(4, at = c(-10,0,10,20), col = "red")
+mtext("Temp. [deg. C]", 4, at = 10, col = "red", line = 3)
+dev.off()
