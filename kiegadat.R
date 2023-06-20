fcc6015339cc6b417085caf444008903bd66b972
Péter Kalicz <kaliczp@gmail.com>
2023-06-20 07:30:03 +0200

    First gap in 2020 determined and filled

--- kiegadat.R
+++ kiegadat.R
@@ -9,3 +9,14 @@ kieg.xts <- xts(kieg[,-1], kieg[,1])
 ## Two gaps
 plot.zoo(kieg.xts['2020-01/2020-04',1])
 lines(as.zoo(tv.xts)[,1], col = 3)
+
+## Height difference gap1
+tail(tv.xts['2020-01-28'])
+kieg.xts['2020-01-28 18:14:59',1]
+head(tv.xts['2020-02-14',1])
+kieg.xts['2020-02-14 08:59:59',1]
+plot.zoo(kieg.xts['2020-01-28/2020-02-15',1]+0.040439)
+lines(as.zoo(tv.xts)[,1], col = 3)
+gap1.xts <- kieg.xts['2020-01-28 18:29:59/2020-02-14 08:44:59']
+gap1.xts[,1] <- gap1.xts[,1] + 0.040439
+tv.xts <- c(tv.xts, gap1.xts)
