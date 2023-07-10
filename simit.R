bd3385bc89ece233da8e0a2f15df97f0751b73b3
Péter Kalicz <kaliczp@gmail.com>
2023-07-10 13:37:09 +0200

    Create all windows plot

--- simit.R
+++ simit.R
@@ -4,22 +4,25 @@ raw.interv <- read_excel("Nyugalmi időszak dátumok Péternek.xlsx")
 interv.only <- na.omit(raw.interv[,2:3, drop = TRUE])
 interv.asc <- data.frame(From = as.character(as.Date(interv.only[,1, drop = TRUE])),
                          To = as.character(as.Date(interv.only[,2, drop = TRUE])))
+
+## plot
+pdf(width = 42/2.54, height = 29.7/2.54)
+par(mfrow = c(3,3))
 ## Given time intervall
-tti <- 1
-oak.tst <- tv.xts$Oak[paste(interv.asc[tti,1],interv.asc[tti,2], sep = "/")]
+for(tti in 1:nrow(interv.asc)) {
+    from.to <- paste(interv.asc[tti,1],interv.asc[tti,2], sep = "/")
+    oak.tst <- tv.xts$Oak[from.to]
 oak.tst.smth <- filter(coredata(oak.tst), filter = rep(1/13,13))
 oak.tst.diff <- diff(oak.tst.smth)
-plot(oak.tst.diff)
-axis(2,0,tck=1, lab = FALSE)
+## plot(oak.tst.diff)
+## axis(2,0,tck=1, lab = FALSE)
 oak.tst.smth[which(oak.tst.diff<0)]
 
 oak.smth.xts <- xts(oak.tst.smth, index(oak.tst))
 
-## plot
-pdf(width = 14)
-plot.zoo(oak.tst, xaxs = "i")
-lines(as.zoo(oak.smth.xts), col = 2)
+    plot.zoo(oak.tst, xaxs = "i", main = from.to)
+    lines(as.zoo(oak.smth.xts), col = "magenta")
+}
 dev.off()
-
 
 plot(tv.xts$Controll['2018-11-01/2019-04-01'])
