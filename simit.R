7e7715e1d9348ffbc22247a0d2175960ea10bdd9
Péter Kalicz <kaliczp@gmail.com>
2023-07-12 18:02:22 +0200

    New plot with different time windows of the selected day

--- simit.R
+++ simit.R
@@ -5,6 +5,10 @@ interv.only <- na.omit(raw.interv[,2:3, drop = TRUE])
 interv.asc <- data.frame(From = as.character(as.Date(interv.only[,1, drop = TRUE])),
                          To = as.character(as.Date(interv.only[,2, drop = TRUE])))
 
+## All day selecte
+interv.day <- as.Date(raw.interv[,1, drop = TRUE])
+
+
 ## plot
 pdf(width = 42/2.54, height = 29.7/2.54)
 par(mfrow = c(3,3))
@@ -33,3 +37,24 @@ simit <- function(x, fromto, wwidth = 13, diff.plot = FALSE) {
     }
     xts(filtered, index(windowed))
 }
+
+pdf(width = 35)
+for(tti in 1:length(interv.day)) {
+    longfrom.to <- paste(interv.day[tti]-1,interv.day[tti]+6, sep = "/")
+    longterm.xts <- tv.xts[longfrom.to,1]
+    long.rang <- range(longterm.xts)
+    from.to <- paste(interv.day[tti]-1,interv.day[tti]+1, sep = "/")
+    simitott <- simit(tv.xts[,1], from.to)
+    par(fig = c(0,0.2,0,1))
+    plot.zoo(tv.xts[as.character(interv.day[tti]),1], xaxs = "i", main = interv.day[tti])
+    lines(as.zoo(simitott), col = "magenta")
+    par(fig = c(0.2,0.6,0,1), new = TRUE)
+    plot.zoo(tv.xts[from.to,1], xaxs = "i", main = from.to, xaxs = "i", ylim = long.rang)
+    lines(as.zoo(simitott), col = "magenta")
+    axis(1, at = as.POSIXct(c(interv.day[tti],interv.day[tti]+1)), tck = 1, col = "lightgray", lab = FALSE)
+    par(fig = c(0.6,1,0,1), new = TRUE)
+    plot.zoo(longterm.xts, xaxs = "i", main = longfrom.to, xaxs = "i", ylim = long.rang)
+    lines(as.zoo(simitott), col = "magenta")
+    axis(1, at = as.POSIXct(c(interv.day[tti],interv.day[tti]+1)), tck = 1, col = "lightgray", lab = FALSE)
+}
+dev.off()
