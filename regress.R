d2e12051ec20bd4b8f038bf1a64507633140c896
Péter Kalicz <kaliczp@gmail.com>
2023-07-14 20:51:56 +0200

    10 days example recharge regression lines

--- regress.R
+++ regress.R
@@ -11,8 +11,10 @@ recharge*60*60*24*1000
 plot(interv.day,recharge*60*60*24*1000)
 axis(2,0,tck=1)
 
-plot.zoo(tv.xts['2018-11-01/2018-11-03',1])
-tti <- 1
+recharge[1:10]*60*60*24*1000
+plot.zoo(tv.xts['2018-11-01/2018-11-10',1])
+for(tti in 1:10) {
 points(as.POSIXct(interv.day[tti]),as.numeric(coredata(tv.xts[as.character(interv.day[tti]),1][1])))
 points(as.POSIXct(interv.day[tti]+1),recharge[tti]*60*60*24+as.numeric(coredata(tv.xts[as.character(interv.day[tti]),1][1])))
 lines(c(as.POSIXct(interv.day[tti]),as.POSIXct(interv.day[tti]+1)),c(as.numeric(coredata(tv.xts[as.character(interv.day[tti]),1][1])),recharge[tti]*60*60*24+as.numeric(coredata(tv.xts[as.character(interv.day[tti]),1][1]))))
+}
