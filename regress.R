70c9de386cb761c2687d0b88b4171c3261f2da9f
Péter Kalicz <kaliczp@gmail.com>
2023-07-19 14:27:34 +0200

    Recharge corrected in the object for mm

--- regress.R
+++ regress.R
@@ -6,15 +6,15 @@ for(tti in 1:length(interv.day)) {
     longterm.lm <- lm(coredata(longterm.xts) ~ as.numeric(index(longterm.xts)))
     recharge <- c(recharge, as.numeric(coefficients(longterm.lm)[2]))
 }
-recharge*60*60*24*1000
+recharge <- recharge*60*60*24*1000
 
-plot(interv.day,recharge*60*60*24*1000)
+plot(interv.day,recharge)
 axis(2,0,tck=1)
 
-recharge[1:10]*60*60*24*1000
+recharge[1:10]
 plot.zoo(tv.xts['2018-11-01/2018-11-10',1])
 for(tti in 1:10) {
 points(as.POSIXct(interv.day[tti]),as.numeric(coredata(tv.xts[as.character(interv.day[tti]),1][1])))
-points(as.POSIXct(interv.day[tti]+1),recharge[tti]*60*60*24+as.numeric(coredata(tv.xts[as.character(interv.day[tti]),1][1])))
-lines(c(as.POSIXct(interv.day[tti]),as.POSIXct(interv.day[tti]+1)),c(as.numeric(coredata(tv.xts[as.character(interv.day[tti]),1][1])),recharge[tti]*60*60*24+as.numeric(coredata(tv.xts[as.character(interv.day[tti]),1][1]))))
+points(as.POSIXct(interv.day[tti]+1),recharge[tti]/1000+as.numeric(coredata(tv.xts[as.character(interv.day[tti]),1][1])))
+lines(c(as.POSIXct(interv.day[tti]),as.POSIXct(interv.day[tti]+1)),c(as.numeric(coredata(tv.xts[as.character(interv.day[tti]),1][1])),recharge[tti]/1000+as.numeric(coredata(tv.xts[as.character(interv.day[tti]),1][1]))))
 }
