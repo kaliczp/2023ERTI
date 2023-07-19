87ec3aecdf290050220b53e218a4f213637e94d6
Péter Kalicz <kaliczp@gmail.com>
2023-07-19 14:38:44 +0200

    Simmetric window

--- regress.R
+++ regress.R
@@ -1,7 +1,7 @@
 recharge <- numeric()
 ## Based on simit.R
 for(tti in 1:length(interv.day)) {
-    longfrom.to <- paste(interv.day[tti]-1,interv.day[tti]+6, sep = "/")
+    longfrom.to <- paste(interv.day[tti]-3,interv.day[tti]+3, sep = "/")
     longterm.xts <- tv.xts[longfrom.to,1]
     longterm.lm <- lm(coredata(longterm.xts) ~ as.numeric(index(longterm.xts)))
     recharge <- c(recharge, as.numeric(coefficients(longterm.lm)[2]))
@@ -18,3 +18,5 @@ points(as.POSIXct(interv.day[tti]),as.numeric(coredata(tv.xts[as.character(inter
 points(as.POSIXct(interv.day[tti]+1),recharge[tti]/1000+as.numeric(coredata(tv.xts[as.character(interv.day[tti]),1][1])))
 lines(c(as.POSIXct(interv.day[tti]),as.POSIXct(interv.day[tti]+1)),c(as.numeric(coredata(tv.xts[as.character(interv.day[tti]),1][1])),recharge[tti]/1000+as.numeric(coredata(tv.xts[as.character(interv.day[tti]),1][1]))))
 }
+
+write.table(data.frame(Date=interv.day, Rech = round(recharge,3)), "Rech_3_3.csv", row = FALSE)
