8c45ce02c1311cdd88fd26ac9727adccd18fa802
Péter Kalicz <kaliczp@gmail.com>
2023-07-25 14:12:54 +0200

    Smoothing for regression prepared based on simit.R

---
+++ smoothregr.R
@@ -0,0 +1,66 @@
+plot.recharge(x = tv.xts[,1], recharge.xts['2018-11-01/2019-05-01'])
+plot.recharge(x = tv.xts[,1], recharge.xts['2019-11-01/2020-05-01'])
+plot.recharge(x = tv.xts[,1], recharge.xts['2020-11-01/2021-05-01'])
+plot.recharge(x = tv.xts[,1], recharge.xts['2021-11-01/2022-05-01'])
+
+plot.recharge(x = tv.xts[,1], recharge.xts['2018-11-01/2018-12-01'])
+plot.recharge(x = tv.xts[,1], recharge.xts['2018-12-01/2019-05-01'])
+plot.recharge(x = tv.xts[,1], recharge.xts['2019-11-01/2019-11-20'])
+plot.recharge(x = tv.xts[,1], recharge.xts['2019-12-01/2020-01-01'])
+plot.recharge(x = tv.xts[,1], recharge.xts['2020-01-01/2020-05-01'])
+plot.recharge(x = tv.xts[,1], recharge.xts['2020-11-01/2021-05-01'])
+plot.recharge(x = tv.xts[,1], recharge.xts['2021-11-01/2021-12-31'])
+plot.recharge(x = tv.xts[,1], recharge.xts['2022-01-01/2022-05-01'])
+
+## 3.25 hours
+sim3hr.xts <- simit(tv.xts[,1], wwidth = 13)
+## 6.25 hours
+sim6hr.xts <- simit(tv.xts[,1], wwidth = 25)
+## 1 day
+sim1day.xts <- simit(tv.xts[,1], wwidth = 97)
+## 3 days
+sim3day.xts <- simit(tv.xts[,1], wwidth = 289)
+## 7 days
+sim7day.xts <- simit(tv.xts[,1], wwidth = 673)
+## 10 days
+sim10day.xts <- simit(tv.xts[,1], wwidth = 961)
+## 14 days
+sim14day.xts <- simit(tv.xts[,1], wwidth = 1344)
+
+
+pdf(width = 35)
+                                        #for(tti in 1:length(interv.day)) {
+for(tti in 1:10) {
+    longfrom.to <- paste(interv.day[tti]-5,interv.day[tti]+6, sep = "/")
+    longterm.xts <- tv.xts[longfrom.to,1]
+    long.rang <- range(longterm.xts)
+    long.rang[1] <- trunc(long.rang[1]*10)/10 - 0.1
+    long.rang[2] <- long.rang[1] + 0.2
+    from.to <- paste(interv.day[tti]-1,interv.day[tti]+1, sep = "/")
+    par(fig = c(0,0.25,0,1))
+    plot.recharge(tv.xts[from.to,1], recharge.xts[from.to], xaxs = "i", main = from.to, xaxs = "i", xlab ="")
+    lines(as.zoo(sim3hr.xts), col = "magenta")
+    par(fig = c(0.25,0.5,0,1), new = TRUE)
+    plot.recharge(tv.xts[from.to,1], recharge.xts[from.to], xaxs = "i", main = from.to, xaxs = "i", ylim = long.rang, xlab = "")
+    lines(as.zoo(sim3hr.xts), col = "magenta")
+    lines(as.zoo(sim6hr.xts), col = "red")
+    lines(as.zoo(sim1day.xts), col = "green")
+    lines(as.zoo(sim3day.xts), col = "blue")
+    lines(as.zoo(sim7day.xts), col = "darkred")
+    lines(as.zoo(sim10day.xts), col = "darkgreen")
+    lines(as.zoo(sim14day.xts), col = "darkblue")
+    lines(as.zoo(simitott), col = "magenta")
+    axis(1, at = as.POSIXct(c(interv.day[tti],interv.day[tti]+1)), tck = 1, col = "lightgray", lab = FALSE)
+    par(fig = c(0.5,1,0,1), new = TRUE)
+    plot.zoo(longterm.xts, xaxs = "i", main = longfrom.to, xaxs = "i", ylim = long.rang, xlab = "")
+    lines(as.zoo(sim3hr.xts), col = "magenta")
+    lines(as.zoo(sim6hr.xts), col = "red")
+    lines(as.zoo(sim1day.xts), col = "green")
+    lines(as.zoo(sim3day.xts), col = "blue")
+    lines(as.zoo(sim7day.xts), col = "darkred")
+    lines(as.zoo(sim10day.xts), col = "darkgreen")
+    lines(as.zoo(sim14day.xts), col = "darkblue")
+    lines(as.zoo(simitott), col = "magenta")
+    axis(1, at = as.POSIXct(c(interv.day[tti],interv.day[tti]+1)), tck = 1, col = "lightgray", lab = FALSE)
+}
+dev.off()
