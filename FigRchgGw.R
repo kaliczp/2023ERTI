46b25c8ec672c19e648a2537e10219c7015cc408
Péter Kalicz <kaliczp@gmail.com>
2023-07-27 06:59:27 +0200

    Plot enhancement
    Main title is added based on table name, axes label are added. Optional selection.

--- FigRchgGw.R
+++ FigRchgGw.R
@@ -4,4 +4,6 @@ plot(FigRechGw[,4:5])
 plot(FigRechGw[,c(3,5)])
 
 ## Delta gw with smoothed
-plot(FigRechGw[,3, drop = TRUE], recharge.xts[,"s3hr"] * 0.0932)
+akt.name <- "s3hr"
+plot(FigRechGw[,3, drop = TRUE], recharge.xts[,akt.name] * 0.0932, main = akt.name,
+     ylab = "Recharge (mm)", xlab = "delta GWL (m)")
