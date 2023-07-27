b819ad128f0237ebbd0663736f1fe11843ad3cfc
Péter Kalicz <kaliczp@gmail.com>
2023-07-27 06:54:24 +0200

    Rech vs. deltaGW figure based on smoothed data
    Test with 3 hours smoothed data

--- FigRchgGw.R
+++ FigRchgGw.R
@@ -2,3 +2,6 @@ library(readxl)
 FigRechGw <- read_excel("Recharge vs GW dGW_erdő_NV simított vs. simítás előtti.xlsx")
 plot(FigRechGw[,4:5])
 plot(FigRechGw[,c(3,5)])
+
+## Delta gw with smoothed
+plot(FigRechGw[,3, drop = TRUE], recharge.xts[,"s3hr"] * 0.0932)
