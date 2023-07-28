bc4370b90d9e171fcf7f0d20b5e584002acbd4e4
Péter Kalicz <kaliczp@gmail.com>
2023-07-28 13:42:42 +0200

    Plotted version of recharge figure

--- FigRchgGw.R
+++ FigRchgGw.R
@@ -4,6 +4,8 @@ plot(FigRechGw[,4:5])
 plot(FigRechGw[,c(3,5)])
 
 ## Delta gw with smoothed
-akt.name <- "s3hr"
+pdf(width = 10)
+for(akt.name in names(recharge.xts))
 plot(FigRechGw[,3, drop = TRUE], recharge.xts[,akt.name] * 0.0932, main = akt.name,
      ylab = "Recharge (mm)", xlab = "delta GWL (m)")
+dev.off()
