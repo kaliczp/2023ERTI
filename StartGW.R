b1eb54c83a8b6c04d96151c70ff19c3a8e27de19
Péter Kalicz <kaliczp@gmail.com>
2023-08-23 11:27:05 +0200

    Regression lines added for whole and greater than -8.6 cases

--- StartGW.R
+++ StartGW.R
@@ -12,6 +12,9 @@ plot(Start.chron, xlab = "")
 
 StartGW.df <- data.frame(GW = StartGW.raw[, 2, drop = TRUE], Time = Start.chron)
 
+StartGW.lm  <- lm(Time ~ GW, data = StartGW.df)
+StartGW2.lm  <- lm(Time ~ GW, data = StartGW.df[StartGW.df$GW > -8.6,])
+
 Plotchron <- times(c("06:30:00", paste(seq(7,23, by = 3), "30:00", sep = ":"), "23:59:59"))
 plot(Time ~ GW, data = StartGW.df,
      xlab = "Groundwater level (m)",
@@ -20,3 +23,5 @@ plot(Time ~ GW, data = StartGW.df,
      ylab = "")
 mtext("Start time of daily recharge period", side = 2, line = 3)
 axis(2, at = Plotchron[- c(1,length(Plotchron))], lab = paste0(c(23, seq(from = 2, to = 14, by = 3)),":00"))
+abline(StartGW.lm)
+abline(StartGW2.lm, col = "grey")
