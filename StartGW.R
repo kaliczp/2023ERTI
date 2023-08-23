e19fd0dd33ddc206f2d1dd4f1f253ceb2c250b44
Péter Kalicz <kaliczp@gmail.com>
2023-08-23 11:19:23 +0200

    Labels are constructed for chron y-axis

--- StartGW.R
+++ StartGW.R
@@ -12,10 +12,11 @@ plot(Start.chron, xlab = "")
 
 StartGW.df <- data.frame(GW = StartGW.raw[, 2, drop = TRUE], Time = Start.chron)
 
-Plotchron <- times(c("06:30:00", "23:59:59"))
+Plotchron <- times(c("06:30:00", paste(seq(7,23, by = 3), "30:00", sep = ":"), "23:59:59"))
 plot(Time ~ GW, data = StartGW.df,
      xlab = "Groundwater level (m)",
      ylim = Plotchron[c(1,length(Plotchron))],
      yaxt = "n", yaxs = "i",
      ylab = "")
 mtext("Start time of daily recharge period", side = 2, line = 3)
+axis(2, at = Plotchron[- c(1,length(Plotchron))], lab = paste0(c(23, seq(from = 2, to = 14, by = 3)),":00"))
