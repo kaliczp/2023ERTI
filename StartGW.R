c28e24ca38d7670ffa6b63093eafa30a7fd416da
Péter Kalicz <kaliczp@gmail.com>
2023-08-23 10:53:46 +0200

    Data.frame is organised from preprocessed data

--- StartGW.R
+++ StartGW.R
@@ -10,7 +10,9 @@ Start.shift <- StartGW.raw[,1, drop = TRUE] + 8.5 * 60 * 60
 Start.chron <- times(format(Start.shift, "%H:%M:%S"))
 plot(Start.chron, xlab = "")
 
-plot(StartGW.raw[, 2, drop = TRUE], Start.chron,
+StartGW.df <- data.frame(GW = StartGW.raw[, 2, drop = TRUE], Time = Start.chron)
+
+plot(Time ~ GW, data = StartGW.df,
      xlab = "Groundwater level (m)",
      yaxt = "n",
      ylab = "")
