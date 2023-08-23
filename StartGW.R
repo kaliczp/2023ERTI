872c4118ef0431d2e00597ad15c42679a703e306
Péter Kalicz <kaliczp@gmail.com>
2023-08-23 10:46:15 +0200

    Axes titles are modified, y axis disabled

--- StartGW.R
+++ StartGW.R
@@ -10,4 +10,8 @@ Start.shift <- StartGW.raw[,1, drop = TRUE] + 8.5 * 60 * 60
 Start.chron <- times(format(Start.shift, "%H:%M:%S"))
 plot(Start.chron, xlab = "")
 
-plot(StartGW.raw[, 2, drop = TRUE], Start.chron)
+plot(StartGW.raw[, 2, drop = TRUE], Start.chron,
+     xlab = "Groundwater level (m)",
+     yaxt = "n",
+     ylab = "")
+mtext("Start time of daily recharge period", side = 2, line = 3)
