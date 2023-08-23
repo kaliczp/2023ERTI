bd0584805b68391d3f597b97335ab65cb12138a6
Péter Kalicz <kaliczp@gmail.com>
2023-08-23 09:44:07 +0200

    Chron package test

--- StartGW.R
+++ StartGW.R
@@ -1,3 +1,9 @@
 library(readxl)
 StartGW.raw <- read_excel("Rech. time vs GW.xlsx")
 plot(StartGW.raw)
+
+library(chron)
+
+Start.chron <- times(format(StartGW.raw[,1, drop = TRUE], "%H:%M:%S"))
+
+plot(StartGW.raw[, 2, drop = TRUE], Start.chron)
