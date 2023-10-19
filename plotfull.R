772e0ad1e62d50d08b3fee802f2b194c4d32d0ae
Péter Kalicz <kaliczp@gmail.com>
2023-10-19 06:31:58 +0200

    Switchable time axis, x-axis
    With new parameter xaxs.default the time axis is optional.

--- plotfull.R
+++ plotfull.R
@@ -2,7 +2,7 @@ Sys.setenv(TZ = "UTC")
 min(tv.xts[,1], na.rm = TRUE);max(tv.xts[,2], na.rm = TRUE)
 datelim.full <- c(ISOdate(2018,11,1, tz = "UTC"), ISOdate(2022,10,10, tz = "UTC"))
 
-plot.fullgw <- function(gw, met, outfile = NULL, datelim, temp = TRUE) {
+plot.fullgw <- function(gw, met, outfile = NULL, datelim, temp = TRUE, xaxs.default = TRUE) {
     require(xts)
     if(!is.null(outfile)) {
         pdf(outfile, width = 14)
@@ -10,11 +10,15 @@ plot.fullgw <- function(gw, met, outfile = NULL, datelim, temp = TRUE) {
 par(mar = c(3.1, 4.1, 0.6, 4.1))
 plot.zoo(gw[,1],
          xaxs = "i", yaxs = "i",
+         xaxt = "n",
          xlab = "", ylab = "GW [m]",
          xlim = datelim, ylim = c(-8.76, -2)
          )
 lines(as.zoo(gw[,2]),
       col = 2)
+    if(xaxs.default) {
+        axis.POSIXct(1)
+    }
 legend("bottomleft", legend = c("Oak", "Contr."), lwd = 1, col = 1:2)
 par(new = TRUE)
 plot.zoo(met[,3], type = "h", lend = 2,
@@ -50,3 +54,5 @@ datelim.long <- c(ISOdate(2015,11,1, tz = "UTC"), ISOdate(2022,10,08, tz = "UTC"
 plot.fullgw(gw = tvlong.xts, met = tvlong.xts, outfile = "GwLongfull.pdf", datelim = datelim.long)
 
 plot.fullgw(gw = tvlongfill.xts, met = tvlongfill.xts, outfile = "GwLongfullfill.pdf", datelim = datelim.long)
+
+plot.fullgw(gw = tvlongfill.xts, met = tvlongfill.xts, outfile = "GwLongfullfill.pdf", datelim = datelim.long, xaxs.default = FALSE)
