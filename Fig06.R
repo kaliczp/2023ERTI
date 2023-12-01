94b6741d1ec719bfa543c73e36d1e3ce3d169753
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2023-12-01 16:09:13 +0100

    Compile plot and save image
    Original factors are modified, to better simililarity. Create ggplot with style. Save as 300 dpi figure.

--- Fig06.R
+++ Fig06.R
@@ -11,5 +11,30 @@ Fig06 <- rbind(Fig06, data.frame(Season = raw06[,1, drop = TRUE],
                                  )
                )
 Fig06 <- cbind(Plot = rep(c("Oak", "Control"), each = nrow(raw06)), Fig06)
-Fig06$Plot <- factor(Fig06$Plot)
-Fig06$Plot <- factor(Fig06$Season)
+Fig06$Plot <- factor(Fig06$Plot, levels = c("Oak", "Control"), ordered = TRUE)
+Fig06$Season <- factor(Fig06$Season, labels = c("Dormant period", "Vegetative period"))
+
+## Plot
+library(ggplot2)
+
+gg06 <- ggplot(data = Fig06, aes(x = Plot, y = Recharge)) +
+    geom_boxplot(aes(fill=Plot)) +
+    scale_fill_manual(values = c(Oak = '#4d99e6', 
+                                 Control = '#cc0033')
+                      ) +
+    facet_wrap(. ~ Season, strip.position = "bottom") +
+    xlab(NULL) + 
+    ylab("Recharge (mm/day)") +
+    theme_minimal() +
+    theme(panel.grid.major.x = element_blank(),
+          axis.title.y = element_text(face = "bold"),
+          axis.text.x=element_blank(),
+          legend.title = element_blank()
+          )
+gg06
+
+png("Fig06R.png", width = 14.25, height = 14.25, units = "cm", res = 300)
+gg06
+dev.off()
+
+
